249f160d6dd2da1479cf930daaa800eedf09a3f7
rolandohuber <rolandohuber@hotmail.com>
2014-07-08 00:16:17 -0300

    Commit inicial hibernate mapeo de herencia, falta resolver generacion de ids

---
+++ EntityInheritance/src/main/java/entity/Persona.java
@@ -0,0 +1,45 @@
+package entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public class Persona extends entity.Entity implements Serializable {
+	private static final long serialVersionUID = -3481041518539559833L;
+
+	private String lastName;
+	private String dni;
+	private Integer telefone;
+
+	public Persona() {
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public Integer getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(Integer telefone) {
+		this.telefone = telefone;
+	}
+
+}
