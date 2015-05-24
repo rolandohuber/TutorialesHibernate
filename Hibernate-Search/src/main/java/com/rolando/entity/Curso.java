fe9c81efa1e03ec412bd031d2da6082bb22e5ba4
Toshiba <Toshiba@192.168.1.73>
2015-05-23 22:34:29 -0300

    se cambiaron las entidades con las que se va a realizar el ejemplo

---
+++ Hibernate-Search/src/main/java/com/rolando/entity/Curso.java
@@ -0,0 +1,54 @@
+package com.rolando.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Curso {
+
+	public static final String ESTUDIANTES_ATTRIBUTE = "estudiantes";
+	private Long id;
+	private String nombre;
+	private Set<Estudiante> estudiantes;
+
+	public Curso() {
+		this.estudiantes = new HashSet<Estudiante>();
+	}
+
+	@Id
+	@GeneratedValue
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@ManyToMany(fetch = FetchType.LAZY, targetEntity = Estudiante.class, mappedBy = Estudiante.CURSOS_ATTRIBUTE)
+	public Set<Estudiante> getEstudiantes() {
+		return estudiantes;
+	}
+
+	public void setEstudiantes(Set<Estudiante> estudiantes) {
+		this.estudiantes = estudiantes;
+	}
+
+	public void addEstudiante(Estudiante estudiante) {
+		this.estudiantes.add(estudiante);
+	}
+}
--- Hibernate-Search/src/main/java/com/rolando/main/Principal.java
+++ Hibernate-Search/src/main/java/com/rolando/main/Principal.java
@@ -1,38 +1,53 @@
 package com.rolando.main;
 
-import java.util.Date;
-
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import com.rolando.entity.Cliente;
+
+import com.rolando.entity.Curso;
+import com.rolando.entity.Estudiante;
 import com.rolando.util.HibernateUtil;
 
 public class Principal {
 	public static void main(String[] args) {
-		
-		Session session = HibernateUtil.getSessionfactory().openSession();
-		Transaction transaction = null;
-		try {
-			transaction = session.beginTransaction();
 
-			Cliente cliente = new Cliente();
-			cliente.setNombre("Juan");
-			cliente.setSaldo(100D);
-			cliente.setApellido("dc");
-			cliente.setCuit("0-0-0");
-			cliente.setFechaAlta(new Date());
-			cliente.setNombreUsuario("pepe");
-			Long id = (Long) session.save(cliente);
-			System.out.println("Id generado " + id);
+		SessionFactory sessionFactory = HibernateUtil.getSessionfactory();
+		Session session = sessionFactory.openSession();
+		Transaction transaction = session.beginTransaction();
 
-			transaction.commit();
-		} catch (Exception e) {
-			transaction.rollback();
-			session.close();
-			HibernateUtil.getSessionfactory().close();
-		}
+		Estudiante estudiante1 = new Estudiante();
+		estudiante1.setName("Estudiante 1");
+		session.save(estudiante1);
+
+		Estudiante estudiante2 = new Estudiante();
+		estudiante2.setName("Estudiante 2");
+		session.save(estudiante2);
+
+		Curso curso = new Curso();
+		curso.setNombre("Programacion I");
+		session.save(curso);
+
+		curso.addEstudiante(estudiante1);
+		curso.addEstudiante(estudiante2);
+
+		session.update(curso);
+
+		estudiante1.addCurso(curso);
+		estudiante2.addCurso(curso);
+
+		session.update(estudiante1);
+		session.update(estudiante2);
+
+		Curso curso2 = new Curso();
+		curso2.setNombre("Programacion II");
+		curso2.addEstudiante(estudiante1);
+		session.save(curso2);
+
+		estudiante1.addCurso(curso2);
+		session.update(estudiante1);
+
+		transaction.commit();
 		session.close();
-		HibernateUtil.getSessionfactory().close();
+
 	}
-	
 }
